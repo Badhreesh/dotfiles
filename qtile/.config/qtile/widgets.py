887a9755d9e945bdcb8bceec67fc3725e8d6a1e3
Badhreesh <rao@grandperspective.de>
2024-08-25 13:39:55 +0200

    Update backlight_name dir for brightness widget

--- qtile/.config/qtile/widgets.py
+++ qtile/.config/qtile/widgets.py
@@ -11,14 +11,15 @@ colours = [
     ["#4fa6ed", "#4fa6ed"],
     ["#bf68d9", "#bf68d9"],
     ["#48b0bd", "#48b0bd"],
-    ["#eff542", "#eff542"] # yellow
+    ["#eff542", "#eff542"],  # yellow
 ]
+
 
 def image() -> widget.Image:
     return widget.Image(
-            filename="~/.config/qtile/ubuntu-logo.png", 
-            mouse_callbacks={"Button1": lazy.next_layout()}
-             )
+        filename="~/.config/qtile/ubuntu-logo.png",
+        mouse_callbacks={"Button1": lazy.next_layout()},
+    )
 
 
 def separator() -> widget.Sep:
@@ -26,7 +27,7 @@ def separator() -> widget.Sep:
 
 
 def group_box() -> widget.GroupBox:
-   return widget.GroupBox(disable_drag=True, highlight_method="line")
+    return widget.GroupBox(disable_drag=True, highlight_method="line")
 
 
 def current_layout_icon() -> widget.CurrentLayoutIcon:
@@ -46,26 +47,36 @@ def systray() -> widget.Systray:
 
 
 def pulse_volume() -> widget.PulseVolume:
-    return widget.PulseVolume(foreground=colours[6], fmt="󰕾 {}", volume_app="pactl", step=5)
+    return widget.PulseVolume(
+        foreground=colours[6], fmt="󰕾 {}", volume_app="pactl", step=5
+    )
 
 
 def brightness() -> widget.Backlight:
-    return widget.Backlight(backlight_name='amdgpu_bl0', change_comand='brightnessctl set {0:.0f}+', fmt='󰃞 {}')
+    """
+    backlight_name is a dir located /sys/class/backlight/
+    """
+    return widget.Backlight(
+        backlight_name="amdgpu_bl1",
+        change_comand="brightnessctl set {0:.0f}+",
+        fmt="󰃞 {}",
+    )
 
 
 def battery() -> widget.Battery:
     return widget.Battery(
-            foreground=colours[4], # Green
-            format="{char}{percent:2.0%}",
-            charge_char="󰂄 ",
-            discharge_char=" ",
-            empty_char=" ",
-            full_char="󰁹 ",
-            unknown_char=" ",
-            low_foreground=colours[3], # Red Background color on low battery
-            low_percentage=0.15, # Show low_forground color at 15% battery
-            show_short_text=False,
-            notify_below=15)
+        foreground=colours[4],  # Green
+        format="{char}{percent:2.0%}",
+        charge_char="󰂄 ",
+        discharge_char=" ",
+        empty_char=" ",
+        full_char="󰁹 ",
+        unknown_char=" ",
+        low_foreground=colours[3],  # Red Background color on low battery
+        low_percentage=0.15,  # Show low_forground color at 15% battery
+        show_short_text=False,
+        notify_below=15,
+    )
 
 
 def clock() -> widget.Clock:
@@ -78,51 +89,49 @@ def exit() -> widget.QuickExit:
 
 def get_widgets_for_laptop():
     widgets = [
-            image(),
-            separator(),
-            group_box(),
-            separator(),
-            current_layout_icon(),
-            current_layout(),
-            separator(),
-            window_name(),
-            separator(),
-            systray(),
-            separator(),
-            pulse_volume(),
-            separator(),
-            brightness(),
-            separator(),
-            battery(),
-            separator(),
-            clock(),
-            separator(),
-            exit()
-            ]
+        image(),
+        separator(),
+        group_box(),
+        separator(),
+        current_layout_icon(),
+        current_layout(),
+        separator(),
+        window_name(),
+        separator(),
+        systray(),
+        separator(),
+        pulse_volume(),
+        separator(),
+        brightness(),
+        separator(),
+        battery(),
+        separator(),
+        clock(),
+        separator(),
+        exit(),
+    ]
     return widgets
 
 
 def get_widgets_for_monitor():
     widgets = [
-            image(),
-            separator(),
-            group_box(),
-            separator(),
-            current_layout_icon(),
-            current_layout(),
-            separator(),
-            window_name(),
-            separator(),
-            pulse_volume(),
-            separator(),
-            brightness(),
-            separator(),
-            battery(),
-            separator(),
-            clock(),
-            separator(),
-            exit()
-            ]
+        image(),
+        separator(),
+        group_box(),
+        separator(),
+        current_layout_icon(),
+        current_layout(),
+        separator(),
+        window_name(),
+        separator(),
+        pulse_volume(),
+        separator(),
+        brightness(),
+        separator(),
+        battery(),
+        separator(),
+        clock(),
+        separator(),
+        exit(),
+    ]
     return widgets
-
-
