af9898c7c36bd728bbce542f2bd78c1202ff4fa1
Badhreesh <rao@grandperspective.de>
2024-09-23 18:21:36 +0200

    Update

--- tmux/tmux_startup.sh
+++ tmux/tmux_startup.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/bash
-
+SESH="tmux_session"
 # Start a new detached tmux session named work
-tmux new-session -d -s work
+tmux new-session -d -s $SESH -n "GP"
 
 # Create "GP" window
-tmux rename-window GP
+# tmux rename-window GP
+
+tmux send-keys -t $SESH:GP "cd projects/gp/scanfeld-pylib" Enter
+tmux send-keys -t $SESH:GP "code ." Enter
+tmux send-keys -t $SESH:GP "clear" C-m
 
 # Create "playground" window
-tmux new-window -t work -dnplayground 
+tmux new-window -t $SESH -dnplayground 
 
 # Attach to the tmux session
-tmux a -t work
+tmux a -t $SESH
 
