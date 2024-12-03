effeb00f15cfc132a1d2882adcc567d57a322b7a
Badhreesh <rao@grandperspective.de>
2024-12-03 20:42:54 +0100

    Add multi external monitor support

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -2,13 +2,12 @@ import os
 import subprocess
 
 from dotenv import load_dotenv
-from libqtile import bar, hook, layout
-from libqtile.config import Click, Drag, Key, Match, Screen
-from libqtile.lazy import lazy
-
 from groups import get_groups
 from keymaps import get_keys
 from layouts import get_layouts
+from libqtile import bar, hook, layout
+from libqtile.config import Click, Drag, Key, Match, Screen
+from libqtile.lazy import lazy
 from widgets import get_widgets_for_laptop, get_widgets_for_monitor
 
 load_dotenv()
@@ -23,10 +22,20 @@ layouts = get_layouts()
 # Extend keys with group related keybindings
 for group_id, group in enumerate(groups, 1):
     # mod1 + letter of group = switch to group
-    switch_to_group = Key([mod], str(group_id), lazy.group[group.name].toscreen(), desc=f"Switch to group {group.name}")
+    switch_to_group = Key(
+        [mod],
+        str(group_id),
+        lazy.group[group.name].toscreen(),
+        desc=f"Switch to group {group.name}",
+    )
 
     # mod1 + shift + letter of group = switch to & move focused window to group
-    switch_and_move_window_to_group = Key([mod, "shift"], str(group_id), lazy.window.togroup(group.name, switch_group=True), desc=f"Switch to & move focused window to group {group.name}")
+    switch_and_move_window_to_group = Key(
+        [mod, "shift"],
+        str(group_id),
+        lazy.window.togroup(group.name, switch_group=True),
+        desc=f"Switch to & move focused window to group {group.name}",
+    )
     keys.extend([switch_to_group, switch_and_move_window_to_group])
 
 widget_defaults = dict(
@@ -40,41 +49,61 @@ connected_monitors = subprocess.run(
     "xrandr | busybox grep -w 'connected' | busybox cut -d' ' -f1",
     capture_output=True,
     shell=True,
-    text=True
-    ).stdout
+    text=True,
+).stdout
 
 
 # todo: Move systray from second monitor to second monitor if available
 laptop_monitor_widgets = get_widgets_for_laptop()
 secondary_screen_widgets = get_widgets_for_monitor()
-home = os.path.expanduser('~')
+home = os.path.expanduser("~")
 
 screens = [
     Screen(
         top=bar.Bar(widgets=laptop_monitor_widgets, size=24, background="#141415"),
         wallpaper=home + "/.config/qtile/walls/Kita_Philosophy.png",
-        wallpaper_mode="fill")
+        wallpaper_mode="fill",
+    )
 ]
 
-external_monitors = connected_monitors.strip().split('\n')[1:] # The first monitor always refers to the laptop screen (eDP)
+external_monitors = connected_monitors.strip().split("\n")[
+    1:
+]  # The first monitor always refers to the laptop screen (eDP)
 for external_monitor in external_monitors:
     screens.append(
         Screen(
-            top=bar.Bar(widgets=secondary_screen_widgets, size=24, background="#141415"),
+            top=bar.Bar(
+                widgets=secondary_screen_widgets, size=24, background="#141415"
+            ),
             wallpaper=home + "/.config/qtile/walls/Kita_Philosophy.png",
-            wallpaper_mode="fill")
+            wallpaper_mode="fill",
         )
-    # Extend monitor from laptop monitor
-    subprocess.run(f"xrandr --output eDP --primary --auto --output {external_monitor} --right-of eDP --auto" , shell=True)
-    # Set laptop speaker as default audio sink (Name got from pactl list sinks)
-    subprocess.run("pactl set-default-sink alsa_output.pci-0000_04_00.6.analog-stereo", shell=True)
-    # Set laptop microphone as default audio source (Name got from pactl list sources)
-    subprocess.run("pactl set-default-source alsa_input.pci-0000_04_00.6.analog-stereo", shell=True)
- 
+    )
+# Extend monitor from laptop monitor (Got the xrandr comand by running arandr)
+subprocess.run(
+    "xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 1920x1200 --pos 3840x0 --rotate normal --output DisplayPort-3 --off --output DisplayPort-4 --mode 1920x1200 --pos 1920x0 --rotate normal --output DisplayPort-5 --off",
+    shell=True,
+)
+# Set laptop speaker as default audio sink (Name got from pactl list sinks)
+subprocess.run(
+    "pactl set-default-sink alsa_output.pci-0000_04_00.6.analog-stereo", shell=True
+)
+# Set laptop microphone as default audio source (Name got from pactl list sources)
+subprocess.run(
+    "pactl set-default-source alsa_input.pci-0000_04_00.6.analog-stereo", shell=True
+)
+
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -119,7 +148,8 @@ wl_input_rules = None
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
 
+
 @hook.subscribe.startup_once
 def start_once():
-    home = os.path.expanduser('~')
-    subprocess.call([home + '/.config/qtile/autostart.sh'])
+    home = os.path.expanduser("~")
+    subprocess.call([home + "/.config/qtile/autostart.sh"])
--- qtile/.config/qtile/keymaps.py
+++ qtile/.config/qtile/keymaps.py
@@ -18,21 +18,40 @@ def get_keys() -> list:
         Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
         Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
         Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-        Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-
+        Key(
+            [mod], "space", lazy.layout.next(), desc="Move window focus to other window"
+        ),
         # Focus screen
-        Key([mod], "comma", lazy.prev_screen(), desc="Move focus to previous screen"),
-        Key([mod], "period", lazy.next_screen(), desc="Move focus to next screen"),
-
+        Key([mod], "period", lazy.prev_screen(), desc="Move focus to previous screen"),
+        Key([mod], "comma", lazy.next_screen(), desc="Move focus to next screen"),
         # Move window
-        Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-        Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+        Key(
+            [mod, "shift"],
+            "h",
+            lazy.layout.shuffle_left(),
+            desc="Move window to the left",
+        ),
+        Key(
+            [mod, "shift"],
+            "l",
+            lazy.layout.shuffle_right(),
+            desc="Move window to the right",
+        ),
         Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
         Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
         # Grow window
-        Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-        Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+        Key(
+            [mod, "control"],
+            "h",
+            lazy.layout.grow_left(),
+            desc="Grow window to the left",
+        ),
+        Key(
+            [mod, "control"],
+            "l",
+            lazy.layout.grow_right(),
+            desc="Grow window to the right",
+        ),
         Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
         Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
         Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -40,36 +59,51 @@ def get_keys() -> list:
         # Split = all windows displayed
         # Unsplit = 1 window displayed, like Max layout, but still with
         # multiple stack panes
-        Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
-
+        Key(
+            [mod, "shift"],
+            "Return",
+            lazy.layout.toggle_split(),
+            desc="Toggle between split and unsplit sides of stack",
+        ),
         # System
         Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
         Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
         Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-
         # Fullscreen
-        Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
-
+        Key(
+            [mod],
+            "f",
+            lazy.window.toggle_fullscreen(),
+            desc="Toggle fullscreen on the focused window",
+        ),
         # Toggles
         # Key([mod], "Tab", lazy.spawn("rofi -show window"), desc="Toggle between windows"),
         Key(["mod1"], "Tab", lazy.group.next_window(), desc="Focus next window"),
         Key([mod], "q", lazy.next_layout(), desc="Toggle between layouts"),
-        Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-
+        Key(
+            [mod],
+            "t",
+            lazy.window.toggle_floating(),
+            desc="Toggle floating on the focused window",
+        ),
         # Apps
         Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-        Key([mod, "control"] , "f", lazy.spawn("rofi -show drun"), desc="Launch rofi"),
+        Key([mod, "control"], "f", lazy.spawn("rofi -show drun"), desc="Launch rofi"),
         Key([mod], "b", lazy.spawn(browser), desc="Launch browser"),
-        
         # Brightness
-        Key([mod], "F5",
+        Key(
+            [mod],
+            "F5",
             lazy.spawn("brightnessctl set 10%-"),
-            desc="Decrease the brightness"),
-        Key([mod], "F6",
+            desc="Decrease the brightness",
+        ),
+        Key(
+            [mod],
+            "F6",
             lazy.spawn("brightnessctl set 10%+"),
-            desc="Increase the brightness"),
+            desc="Increase the brightness",
+        ),
         # todo: Make dedicated set of keymaps for rofi
-
         # Extend to a second monitor
         # Key([mod], "m", lazy.spawn("xrandr --output DisplayPort-3 --mode 1920x1200 --right-of eDP"), desc="Extend display to a second monitor")
         # Run this command to enable using a second monitor: xrandr --output eDP --primary --auto --output DisplayPort-2 --right-of eDP --auto
